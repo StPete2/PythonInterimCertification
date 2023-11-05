38a438e589c20af3172bf15791e9bdb3ca8c981a
Aleksey Eryukhin <Alekseytestworld@gmail.com>
2023-11-05 12:14:27 +0300

    search added, first part of editing

--- decomposition_main.py
+++ decomposition_main.py
@@ -4,7 +4,10 @@ from datetime import datetime
 def Menu():
     print("Введите 1, если хотите создать заметку")
     print("Введите 2, если хотите вывести список заметок")
-    key_count = getKeyCount()
+    print("Введите 3 для поиска заметки по её номеру")
+    print("Введите 4 для редактирования заметки по её номеру")
+    key_count = 0
+    # key_count = getKeyCount()
     phone_dir = dict()
     while True:
         num = input("Введите Ваш выбор: ")
@@ -17,12 +20,25 @@ def Menu():
             break
         elif num == 1:
             user = Input_User()
-            # phone_dir, key_count = Create_User(phone_dir, key_count, user)
             Export_Data(user)
         elif num == 2:
             phone_dir = Import_Data(phone_dir)
             Print_Phone_Dir(phone_dir)
-            break
+        elif num == 3:
+            phone_dir = Import_Data(phone_dir)
+            Print_Phone_Dir(phone_dir)
+            key_count = search_User(phone_dir)
+            print_Selected_Note(phone_dir, key_count)
+        elif num == 4:  # editing a note
+            if key_count != 0:
+                if not asking_a_question():
+                    key_count = search_User(phone_dir)
+                Update_User(phone_dir, key_count)
+            else:
+                key_count = search_User(phone_dir)
+                Update_User(phone_dir, key_count)
+                print("заглушка")
+                # edit a note
 
         else:
             print("Вы ввели некорректное значение")
@@ -52,13 +68,11 @@ def Export_Data(user: list):
 
 
 def Import_Data(phone_dir_local: dict) -> dict:
-    key_count = 1
     file_name = getFileName()
     with open(file_name, mode='rt', encoding='utf-8') as file:
         for line in file:
-            _, heading, body, data = line.strip().split(';')
+            key_count, heading, body, data = line.strip().split(';')
             phone_dir_local[key_count] = [heading, body, data]
-            key_count += 1
     print("Операция чтения заметок выполнена успешно.")
     print()
     return phone_dir_local
@@ -71,6 +85,66 @@ def Print_Phone_Dir(phone_dir_local: dict):
     for key_count, user in phone_dir_local.items():
         print(f"Номер: {key_count}. Заголовок: {user[0]}. Дата: {user[2]}")
     print()
+
+
+def search_User(phone_dir_local: dict) -> int:
+    input_data = input("Введите номер заметки: ")
+    print()
+    if not input_data.isdigit():
+        print("Вы ввели некорректное значение")
+        return
+
+    for key_count_found, user in phone_dir_local.items():
+        if input_data == key_count_found:
+            return key_count_found
+    else:
+        print("Заметки с таким номером не существует.")
+        print()
+        return
+
+
+def asking_a_question() -> bool:
+    reply = str(input("Вы хотите отредактировать только что выбранную заметку? Y/N: ")).capitalize()
+    if reply == 'Y':
+        return True
+    else:
+        return False
+
+
+def Update_User(phone_dir_local: dict, key_count_local: int) -> dict:
+    new_heading = str(input("Введите новое название заметки: "))
+    new_body = str(input("Введите новое содержание заметки: "))
+    new_data = datetime.now().date()
+    update_confirmation = str(input(
+        "Подтвердите внесение изменений, нажав 'Y'. Нажмите 'N' для возврата в главное меню: ")).capitalize()
+    if update_confirmation == 'Y':
+        user = []
+        user.append(new_heading)
+        user.append(new_body)
+        user.append(new_data)
+        phone_dir_local[key_count_local] = user
+        return phone_dir_local
+    else:
+        return phone_dir_local
+
+
+def save_note_after_editing(phone_dir_local: dict, user: list, key_count_local: int):
+    file_name = getFileName()
+    # if not FileExists(file_name):
+    #     print(f"{file_name} не существует, но будет создан")
+
+    with open(file_name, mode='w', encoding='utf-8') as file:
+        file.write(f"{key_count_local};{user[0]};{user[1]};{user[2]}\n")
+    print("Заметка сохранена успешно.")
+    print()
+
+
+def print_Selected_Note(phone_dir_local: dict, input_data: int):
+    for key_count_found, user in phone_dir_local.items():
+        if input_data == key_count_found:
+            print(f"Заметка №{key_count_found}. Заголовок: {user[0]}. Дата: {user[2]}")
+            print(f"Содержание заметки:")
+            print(f"{user[1]}")
 
 
 def FileExists(file_name: str):
